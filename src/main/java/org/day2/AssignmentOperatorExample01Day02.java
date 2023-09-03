00b14efa6b4a022f4ffd0f5ec7d5aa05503e75df
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-03 10:49:28 +0530

    weekly batch commit - 03 sep 2023

--- src/main/java/org/day2/AssignmentOperatorExample01Day02.java
+++ src/main/java/org/day2/AssignmentOperatorExample01Day02.java
@@ -8,5 +8,8 @@ public class AssignmentOperatorExample01Day02 {
         // +=
         x+=5; // x=x+5 -> x=20+5 -> x=25
         System.out.println("Value of x is ->"+x);
+        x-=10;//x=x-10; // x=25-10= x=15
+        System.out.println("Value of x is ->"+x);
+
     }
 }
