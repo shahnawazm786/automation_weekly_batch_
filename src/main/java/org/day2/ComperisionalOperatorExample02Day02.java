ecca64e2174eb10a12b09600bac28ace718dae5c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-03 10:34:51 +0530

    weekly batch commit - 03 sep 2023

---
+++ src/main/java/org/day2/ComperisionalOperatorExample02Day02.java
@@ -0,0 +1,18 @@
+package org.day2;
+
+public class ComperisionalOperatorExample02Day02 {
+    public static void main(String[] args) {
+
+        System.out.println(100>50);
+        boolean flag=100>50;
+        System.out.println(flag);
+        //System.out.println("Dispensable amount");
+        String name="KAZ";
+        System.out.println(name);
+        name="SAM";
+        System.out.println(name);
+
+
+
+    }
+}
--- src/main/java/org/day2/ComperisionalOperatorExampleDay02.java
+++ src/main/java/org/day2/ComperisionalOperatorExampleDay02.java
@@ -18,6 +18,10 @@ public class ComperisionalOperatorExampleDay02 {
         System.out.println(500>=100); // true
         System.out.println(500>=500); // true
         System.out.println(100>=200); // false
-
+//https://github.com/shahnawazm786/automation_weekly_batch_
+        System.out.println("------ <= -----");
+        System.out.println(100<=500); // true
+        System.out.println(500>=500); // true
+        System.out.println(200<=100); // false
     }
 }
