7e4b55faa375a8144c80bb227bc6fe482612a68a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-09 10:10:49 +0530

    weekly batch commit - 09 sep 2023

---
+++ src/main/java/org/day3/CarClassImplementations.java
@@ -0,0 +1,16 @@
+package org.day3;
+
+public class CarClassImplementations {
+    // Car - > Custom Classs
+    public static void main(String[] args) {
+        Car c; // c is the reference of class
+        Car c1; // c1 is the reference of class
+        // To access the class method and attribute
+        // classrefrence.method/attribute;
+        // to create object of the class
+        c=new Car();
+        System.out.println(c.color);
+        System.out.println(c.dt);
+
+    }
+}
