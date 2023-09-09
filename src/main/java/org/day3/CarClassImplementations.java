65554802930c5f81c7f6047b50dc1fd390955ade
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-09 10:23:20 +0530

    weekly batch commit - 09 sep 2023

--- src/main/java/org/day3/CarClassImplementations.java
+++ src/main/java/org/day3/CarClassImplementations.java
@@ -6,7 +6,7 @@ public class CarClassImplementations {
         Car c; // c is the reference of class
         Car c1; // c1 is the reference of class
         // To access the class method and attribute
-        // classrefrence.method/attribute;
+        // class reference.method //attribute;
         // to create object of the class
         c=new Car();
         System.out.println(c.color);
