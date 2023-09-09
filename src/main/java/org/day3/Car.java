6ec053a743bfb79fad3655a08f06a562eb7b4e95
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-09 10:02:19 +0530

    weekly batch commit - 09 sep 2023

--- src/main/java/org/day3/Car.java
+++ src/main/java/org/day3/Car.java
@@ -1,4 +1,18 @@
 package org.day3;
 
+// Blue print of Car
 public class Car {
+    String model_no;
+    int no_doors;
+    int no_seats;
+    String color;
+    public void getStart(){
+        System.out.println("Car has been started.......");
+    }
+    public void getMove(){
+        System.out.println("Car moving now .............");
+    }
+    public void getStop(){
+        System.out.println("Car stopped ...............");
+    }
 }
