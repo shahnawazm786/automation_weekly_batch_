8a2b6441c56c6d31311d2599907498b76941cae9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-09 11:12:10 +0530

    weekly batch commit - 09 sep 2023

---
+++ src/main/java/org/day3/ConditionalConstructSwitchCase.java
@@ -0,0 +1,32 @@
+package org.day3;
+
+public class ConditionalConstructSwitchCase {
+    // chrome
+    // firefox
+    // internet
+    // safari
+    public static void main(String[] args) {
+        String browserName="chrome";
+        switch(browserName){
+            case "chrome":
+                System.out.println("Chrome browser");
+                break;
+            case "firefox":
+                System.out.println("Firefox browser");
+                break;
+            case "internet":
+                System.out.println("Internet explorer");
+                break;
+            case "safari":
+                System.out.println("Safari browser");
+                break;
+            default:
+                System.out.println("Windows browser");
+
+        }
+
+    }
+    // write a program to show weekdays name
+    // write a program to show month name
+
+}
