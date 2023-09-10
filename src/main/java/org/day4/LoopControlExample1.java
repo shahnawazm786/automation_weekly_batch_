b8a069690162d84c304a41bc98a2044e7b7a7c00
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-10 20:29:05 +0530

    weekly batch commit - 09 sep 2023

--- src/main/java/org/day4/LoopControlExample1.java
+++ src/main/java/org/day4/LoopControlExample1.java
@@ -8,6 +8,11 @@ public class LoopControlExample1 {
             System.out.println(x+ "\tGood morning");
             x++; // increment or decrement
         }
+        int y=100;
+        while(y>=1){
+            System.out.println(y+ "\ttimes");
+            y++;
+        }
 
     }
 }
