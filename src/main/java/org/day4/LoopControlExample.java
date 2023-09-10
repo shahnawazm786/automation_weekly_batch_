56aecad54cf344655a9930d6a837b7c3f88ec2a9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-10 20:13:41 +0530

    weekly batch commit - 09 sep 2023

---
+++ src/main/java/org/day4/LoopControlExample.java
@@ -0,0 +1,28 @@
+package org.day4;
+
+public class LoopControlExample {
+    public static void main(String[] args) {
+        // Good morning
+        // 10 times display on console
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+    }
+}
