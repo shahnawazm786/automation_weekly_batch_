34de9d3652656529d7fd13239726a2ebd11bbe41
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-10 21:09:19 +0530

    weekly batch commit - 10 sep 2023

--- src/main/java/org/day4/ObjectForLoopExample.java
+++ src/main/java/org/day4/ObjectForLoopExample.java
@@ -1,4 +1,18 @@
 package org.day4;
 
 public class ObjectForLoopExample {
+    public static void main(String[] args) {
+        /*String name="Java Language"; //Object
+        for(char c : name.toCharArray()){ // for each loop
+            System.out.println(c);
+        }*/
+        String[] lang={"C","Java","C++","Python"};
+        for(String s:lang){
+            System.out.println(s);
+        }
+        int[] num={10,20,30,40};
+        for(int n:num){
+            System.out.println(n);
+        }
+    }
 }
