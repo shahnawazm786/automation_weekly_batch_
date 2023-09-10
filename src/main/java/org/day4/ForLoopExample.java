824493fea45fba4fcc1e810ea02866cdee171421
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-10 20:28:59 +0530

    weekly batch commit - 09 sep 2023

---
+++ src/main/java/org/day4/ForLoopExample.java
@@ -0,0 +1,15 @@
+package org.day4;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+    for(int z=1;z<=100;z++){
+        System.out.println(z + "\t times run");
+    }
+    // second example -> it is similar to while loop
+    int l=1;
+    for(;l<=10;){
+        System.out.println("value of L \t"+l);
+        l++;
+    }
+    }
+}
