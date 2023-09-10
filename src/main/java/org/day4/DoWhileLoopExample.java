78b1c379ee17c6cbcbfd1bb911ed42f741731774
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-10 20:18:21 +0530

    weekly batch commit - 09 sep 2023

---
+++ src/main/java/org/day4/DoWhileLoopExample.java
@@ -0,0 +1,12 @@
+package org.day4;
+
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        int x=100;
+        do {
+            System.out.println(x+ "\tGood morning");
+            x++;
+        }while(x<=97); // 100<=97 => false
+
+    }
+}
