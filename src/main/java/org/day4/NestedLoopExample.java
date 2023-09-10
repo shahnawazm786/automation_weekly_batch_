6b3d2802e683d7087b2bd34b41355805aa408e7c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-10 21:15:15 +0530

    weekly batch commit - 10 sep 2023

---
+++ src/main/java/org/day4/NestedLoopExample.java
@@ -0,0 +1,34 @@
+package org.day4;
+
+public class NestedLoopExample {
+    public static void main(String[] args) {
+        // Nested loop means loop within loop
+        for(int x=1;x<=4;x++){
+            for(int y=1;y<=4;y++){
+                System.out.print(x+"  ");
+            }
+            System.out.println();
+        }
+        System.out.println("********************");
+        for(int x=1;x<=4;x++){
+            for(int y=1;y<=4;y++){
+                System.out.print(y+"  ");
+            }
+            System.out.println();
+        }
+        System.out.println("********************");
+        for(int x=1;x<=4;x++){
+            for(int y=1;y<=x;y++){
+                System.out.print(y+"  ");
+            }
+            System.out.println();
+        }
+        System.out.println("********************");
+        for(int x=1;x<=4;x++){
+            for(int y=x;y>=1;y--){
+                System.out.print(y+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
