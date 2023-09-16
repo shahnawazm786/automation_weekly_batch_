0df7da60d40a31c9be2c1fd5f8abbc1d580547a8
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-16 12:15:31 +0530

    weekly batch commit - 16 sep 2023 For each

--- src/main/java/org/day5/GreaterValueFromArray.java
+++ src/main/java/org/day5/GreaterValueFromArray.java
@@ -1,4 +1,20 @@
 package org.day5;
 
 public class GreaterValueFromArray {
+    public static void main(String[] args) {
+
+        int[] a={40,35,70,-80,7,19}; //second or third
+        int greater=a[0];
+        //greater =40
+
+        for(int i=0;i<a.length;i++){
+            System.out.printf("%d  < %d = %b\n",greater,a[i],greater<a[i]);
+                if(greater<a[i]){
+                    greater=a[i];
+                }
+
+        }
+        System.out.println("Greater value in the array is "+greater);
+
+    }
 }
