be355d0464df4928098f1b6c2d1f032d7d069841
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-16 11:43:58 +0530

    weekly batch commit - 16 sep 2023 For each

---
+++ src/main/java/org/day5/ClassImpl1.java
@@ -0,0 +1,12 @@
+package org.day5;
+
+public class ClassImpl1 {
+    public static void main(String[] args) {
+        Animals k=new Animals();
+        k.color="Brown";
+        System.out.println(k.color);
+        Animals k1=new Animals();
+        System.out.println(k1.color);
+        Animals k2;
+    }
+}
