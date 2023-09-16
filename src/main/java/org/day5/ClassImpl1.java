623f8f1ae575d32a568132fd4ca7f23be1713bb7
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-16 11:52:25 +0530

    weekly batch commit - 16 sep 2023 For each

--- src/main/java/org/day5/ClassImpl1.java
+++ src/main/java/org/day5/ClassImpl1.java
@@ -8,5 +8,15 @@ public class ClassImpl1 {
         Animals k1=new Animals();
         System.out.println(k1.color);
         Animals k2;
+        k2=k;
+        System.out.println(k2.color);
+        if(k2==k)
+            System.out.println("Both are same");
+
+        if(k==k1)
+            System.out.println("Both are same");
+        else
+            System.out.println("Both are not same");
+
     }
 }
