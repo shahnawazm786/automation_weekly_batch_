506bba6d2412b3158dcf5ae345952fed02002c2c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-16 12:15:38 +0530

    weekly batch commit - 16 sep 2023 For each

--- src/main/java/org/day5/IntArrayExample.java
+++ src/main/java/org/day5/IntArrayExample.java
@@ -2,7 +2,7 @@ package org.day5;
 
 public class IntArrayExample {
     public static void main(String[] args) {
-        int[] a={40,60,70,80,7,300};
+        int[] a={40,60,70,80,7,300}; //inline declaration
         System.out.println(a[0]);
         System.out.println(a[4]);
         //System.out.println(a[6]);
@@ -15,5 +15,14 @@ public class IntArrayExample {
         }
         // arrange in ascending order
         // first greater value
+        int[] num= new int[6]; // size 6 define
+        int j=100;
+        for(int i=0;i<num.length;i++){
+            num[i]=j*2-3+1-i*2;
+        }
+        System.out.println("Num Array Value");
+        for (int i=0;i<num.length;i++){
+            System.out.println(num[i]);
+        }
     }
 }
