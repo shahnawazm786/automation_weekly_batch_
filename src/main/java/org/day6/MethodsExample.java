bfbd20523e4cd275bf9f56b9200a1c928bb4d6e1
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-17 11:54:50 +0530

    weekly batch commit - 17 sep 2023 For each

--- src/main/java/org/day6/MethodsExample.java
+++ src/main/java/org/day6/MethodsExample.java
@@ -2,16 +2,21 @@ package org.day6;
 
 public class MethodsExample {
     public static void main(String[] args) {
-
+        System.out.println(getMyName("learning kaz program kaz"));
     }
     public static String getMyName(String name){
-       String nameinuppercase=name.toUpperCase();
-       String subString=nameinuppercase.substring(0,10);
+        String subString;
+        String nameinuppercase=name.toUpperCase();
+        System.out.println("Length ->"+name.length());
+        System.out.println("Index of K"+name.indexOf("k"));
+        System.out.println("Index of K"+name.indexOf("z"));
+        subString=nameinuppercase.substring(9,12);
+        System.out.println(subString);
+        if(subString.equalsIgnoreCase("KAZ"))
+            return "KAZ";
+        else
+            return null;
 
-        if(name.equalsIgnoreCase("KAZ"))
-        return "KAZ";
-       else
-           return null;
     }
 
 }
