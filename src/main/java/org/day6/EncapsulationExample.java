b8c4323519d4b5ce4d551860f9aa03d057b35397
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-17 11:43:00 +0530

    weekly batch commit - 17 sep 2023 For each

--- src/main/java/org/day6/EncapsulationExample.java
+++ src/main/java/org/day6/EncapsulationExample.java
@@ -2,9 +2,10 @@ package org.day6;
 
 public class EncapsulationExample {
     public static void main(String[] args) {
-        StudentClass std = new StudentClass();
-        std.setStudentName("KAZ");
+      StudentClass std=new StudentClass();
+      std.setStudentName("KAZ");
         System.out.println(std.getStudentName());
+
     }
 
 }
