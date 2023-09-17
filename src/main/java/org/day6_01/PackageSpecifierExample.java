62c26d4a8f07d3284da2acf6bb01706a7c45ddbb
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-17 12:15:39 +0530

    weekly batch commit - 17 sep 2023 For each

--- src/main/java/org/day6_01/PackageSpecifierExample.java
+++ src/main/java/org/day6_01/PackageSpecifierExample.java
@@ -1,4 +1,10 @@
 package org.day6_01;
 
+import org.day6.StudentClass;
+
 public class PackageSpecifierExample {
+    public static void main(String[] args) {
+        StudentClass std=new StudentClass();
+
+    }
 }
