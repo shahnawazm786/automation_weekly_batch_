d6af60cdbc8d0d06c47ecd656474379acd5bbd64
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-17 11:42:49 +0530

    weekly batch commit - 17 sep 2023 For each

--- src/main/java/org/day6/StudentClass.java
+++ src/main/java/org/day6/StudentClass.java
@@ -2,7 +2,13 @@ package org.day6;
 // Encapsulation example
 // Data is obtained from the public method only
 public class StudentClass {
-    private String studentName;
+
+    private String studentName;// private access specifier
+
+
+    // Access Specifier -> private, protected, public, package / default
+    // provide accessibility
+
     public String getStudentName() {
         return studentName;
     }
@@ -10,5 +16,13 @@ public class StudentClass {
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
+    public void show(){
+        System.out.println(this.studentName);
+    }
+
+    // Modifier -> final, static, final, abstract
+
+
+
 
 }
