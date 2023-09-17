9bb4e2be99016620a1b393244294740b08894a09
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-17 11:58:07 +0530

    weekly batch commit - 17 sep 2023 For each

--- src/main/java/org/day6/MethodsExample.java
+++ src/main/java/org/day6/MethodsExample.java
@@ -3,6 +3,8 @@ package org.day6;
 public class MethodsExample {
     public static void main(String[] args) {
         System.out.println(getMyName("learning kaz program kaz"));
+        System.out.println(getBiggerNumber(100,50));
+        System.out.println(getDivisions(10,3));
     }
     public static String getMyName(String name){
         String subString;
@@ -17,6 +19,21 @@ public class MethodsExample {
         else
             return null;
 
+    }
+    public static int getBiggerNumber(int a,int b){
+        if(a>b)
+            return a;
+        else
+            return b;
+    }
+    public static double getDivisions(double a,double b){
+        return a/b;
+    }
+    public static boolean getFlag(String name){
+        if(name.length()>0)
+            return true;
+        else
+            return false;
     }
 
 }
