91d2997aef5dab740b20f4b75b7f974f4b7f4015
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-23 12:25:41 +0530

    weekly batch commit - 23 sep 2023 For each

---
+++ src/main/java/org/day7/finalkeyword/ScarpioClass.java
@@ -0,0 +1,12 @@
+package org.day7.finalkeyword;
+
+public class ScarpioClass extends HeroClass{
+    void show7Seat(){
+        System.out.println("7 sear");
+    }
+   /* @Override
+    void noofSeat(){
+        System.out.println("7 seats");
+    }
+*/
+}
