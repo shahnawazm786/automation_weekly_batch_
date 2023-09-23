024a40660b4dd19230d68e5d3945053c65bcd4e9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-23 11:21:47 +0530

    weekly batch commit - 23 sep 2023 For each

---
+++ src/main/java/org/day7/PackageVariableAccessImpl.java
@@ -0,0 +1,8 @@
+package org.day7;
+
+public class PackageVariableAccessImpl {
+    public static void main(String[] args) {
+        PackageVariableAccessibility obj=new PackageVariableAccessibility();
+        obj.roomNo=123;
+    }
+}
