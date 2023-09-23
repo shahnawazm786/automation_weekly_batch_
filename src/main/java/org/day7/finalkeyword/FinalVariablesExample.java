172ee73258947e4bb19fff91ded5f7feac67bc57
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-23 12:24:28 +0530

    weekly batch commit - 23 sep 2023 For each

---
+++ src/main/java/org/day7/finalkeyword/FinalVariablesExample.java
@@ -0,0 +1,9 @@
+package org.day7.finalkeyword;
+
+public class FinalVariablesExample {
+    public static void main(String[] args) {
+        final double pie=3.414;
+        System.out.println(pie);
+        //pie=4.356;
+    }
+}
