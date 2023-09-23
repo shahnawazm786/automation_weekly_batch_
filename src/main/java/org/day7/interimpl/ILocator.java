8c5c9a60c9843b3f31f8bf643f1ad1dc0c86085c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-23 12:25:30 +0530

    weekly batch commit - 23 sep 2023 For each

---
+++ src/main/java/org/day7/interimpl/ILocator.java
@@ -0,0 +1,11 @@
+package org.day7.interimpl;
+
+public interface ILocator {
+     double pie=3.414;
+     double area(int radius);
+
+    private double square(int length) {
+        return length*length;
+    }
+
+}
