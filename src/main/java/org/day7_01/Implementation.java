6101f34c084cada50f1401e2361361e996789b83
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-23 11:21:18 +0530

    weekly batch commit - 23 sep 2023 For each

---
+++ src/main/java/org/day7_01/Implementation.java
@@ -0,0 +1,13 @@
+package org.day7_01;
+
+import org.day7.ProtectedVariableAccessibility;
+
+public class Implementation extends ProtectedVariableAccessibility {
+    public static void main(String[] args) {
+        Implementation p=new Implementation();
+        p.flightNo=120;
+
+
+
+    }
+}
