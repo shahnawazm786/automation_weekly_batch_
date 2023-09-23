32971f2402bab2c98cf05df36d2b2dd82ebc6426
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-09-23 11:21:33 +0530

    weekly batch commit - 23 sep 2023 For each

---
+++ src/main/java/org/day6/pack_01/PackageLeveSpecifierExample.java
@@ -0,0 +1,10 @@
+package org.day6.pack_01;
+
+import org.day6.StudentClass;
+
+public class PackageLeveSpecifierExample {
+    public static void main(String[] args) {
+        StudentClass st=new StudentClass();
+
+    }
+}
