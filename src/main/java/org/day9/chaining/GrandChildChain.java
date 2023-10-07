64e924922110958ec5c809be1fd5ab57267faa66
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-07 11:19:30 +0530

    weekly batch commit - 24 sep 2023 For each

---
+++ src/main/java/org/day9/chaining/GrandChildChain.java
@@ -0,0 +1,7 @@
+package org.day9.chaining;
+
+public class GrandChildChain extends  ChildChain{
+    public GrandChildChain(){
+        System.out.println("Grand Child Chain Constructor");
+    }
+}
