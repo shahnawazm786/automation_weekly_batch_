da1c453a61899c52ffb18a0ddcd8590d758d2396
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-07 10:34:08 +0530

    weekly batch commit - 24 sep 2023 For each

---
+++ src/main/java/org/day9/DemoSingleInheritance.java
@@ -0,0 +1,10 @@
+package org.day9;
+
+public class DemoSingleInheritance {
+    public static void main(String[] args) {
+       // CowClass cow=new CowClass("Red",4,"Moti");
+       // System.out.println(cow.getAnimal());
+        AnimalClass animal=new AnimalClass("Heera");
+        System.out.println(animal.getAnimal());
+    }
+}
