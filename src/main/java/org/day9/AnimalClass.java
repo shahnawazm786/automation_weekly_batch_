b5cbf36104d9578108d96f98f822453f6340a969
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-07 10:33:33 +0530

    weekly batch commit - 24 sep 2023 For each

---
+++ src/main/java/org/day9/AnimalClass.java
@@ -0,0 +1,11 @@
+package org.day9;
+
+public class AnimalClass {
+    String animalName;
+    public AnimalClass(String animalName){
+        this.animalName=animalName;
+    }
+    public String getAnimal(){
+        return this.animalName;
+    }
+}
