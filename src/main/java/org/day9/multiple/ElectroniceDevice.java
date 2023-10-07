4e83756dbf3b30d2b772536be6bb26e4de6e698f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-07 11:19:25 +0530

    weekly batch commit - 24 sep 2023 For each

---
+++ src/main/java/org/day9/multiple/ElectroniceDevice.java
@@ -0,0 +1,9 @@
+package org.day9.multiple;
+
+public class ElectroniceDevice {
+    String name;
+    public String deviceName(String name){
+        this.name=name;
+        return this.name;
+    }
+}
