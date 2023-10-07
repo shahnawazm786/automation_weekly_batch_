240a989858cd62243a8412f73f6d037d3f45e6c9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-07 10:34:03 +0530

    weekly batch commit - 24 sep 2023 For each

---
+++ src/main/java/org/day9/multiple/DemoMulitpleInheritanceUsingInterface.java
@@ -0,0 +1,9 @@
+package org.day9.multiple;
+
+
+public class DemoMulitpleInheritanceUsingInterface implements IMultipleIngheritanceParent1,IMultipleIngheritanceParent2 {
+    public static void main(String[] args) {
+        System.out.println(IMultipleIngheritanceParent1.parentName);
+
+    }
+}
