55bae2ff3b72938117a8efc6c44b6854d9909e57
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-07 11:19:07 +0530

    weekly batch commit - 24 sep 2023 For each

---
+++ src/main/java/org/day9/multiple/Demo.java
@@ -0,0 +1,11 @@
+package org.day9.multiple;
+
+public class Demo extends MobileApps{
+    public static void main(String[] args) {
+        MobileApps mobile=new MobileApps();
+        System.out.println(mobile.deviceName("Samsung - 11"));
+        mobile.deviceDetails();
+
+
+    }
+}
