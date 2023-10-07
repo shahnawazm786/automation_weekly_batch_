0ec55a90a8b92b80a10af81a1669333c05644ddc
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-07 11:20:13 +0530

    weekly batch commit - 24 sep 2023 For each

---
+++ src/main/java/org/day9/multiple/PrivateConstructorImpl.java
@@ -0,0 +1,9 @@
+package org.day9.multiple;
+
+import org.day9.PrivateConstructor;
+
+public class PrivateConstructorImpl {
+    public static void main(String[] args) {
+        //PrivateConstructor p= new PrivateConstructor();
+    }
+}
