85232174db61992425f9cfe0dc61f209564b480c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-07 10:33:38 +0530

    weekly batch commit - 24 sep 2023 For each

---
+++ src/main/java/org/day9/mulitlevel/CarClass.java
@@ -0,0 +1,9 @@
+package org.day9.mulitlevel;
+
+public class CarClass {
+    int noOfSeat;
+    public void setNoOfSeat(int noOfSeat){
+        this.noOfSeat=noOfSeat;
+        System.out.println("No of seat is ->"+this.noOfSeat);
+    }
+}
