def0e34eff8498b88d3e72bca0276ad521f18c13
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-07 10:34:51 +0530

    weekly batch commit - 24 sep 2023 For each

--- src/main/java/org/day7_01/Polymorph.java
+++ src/main/java/org/day7_01/Polymorph.java
@@ -2,22 +2,22 @@ package org.day7_01;
 
 // Method overload
 public class Polymorph {
-    void add(){
+    public void add(){
         System.out.println("Add with no parameter");
     }
-    void add(int a, int b){
+    public void add(int a, int b){
         System.out.println("Add with two integer parameter");
     }
-    void add(double a, int b){
+    public void add(double a, int b){
         System.out.println("Add with two integer parameter");
     }
-    void add(int a, double b){
+    public void add(int a, double b){
         System.out.println("Add with two integer parameter");
     }
-    void add(int a, int b,int c){
+    public void add(int a, int b,int c){
         System.out.println("Add with two integer parameter");
     }
-    void add(double a, int b,int c){
+    public void add(double a, int b,int c){
         System.out.println("Add with two integer parameter");
     }
 }
