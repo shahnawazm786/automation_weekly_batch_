31b2e6939823144b7d6c0f99d64d44c26ea468cb
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-08 10:39:43 +0530

    weekly batch commit - 08 Oct 2023 For each

---
+++ src/main/java/org/day10/Demo3.java
@@ -0,0 +1,7 @@
+package org.day10;
+
+public class Demo3 {
+    public static void main(String[] args) throws CustomeExceptions {
+        throw new CustomeExceptions("Forcely callaed throw excceptions");
+    }
+}
