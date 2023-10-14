500a8877c5150264f04c6493b3021cd48c021de6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-14 10:28:19 +0530

    weekly batch commit - 08 Oct 2023 For each

--- src/main/java/org/day11/files/WriteContentIntoaFileExample.java
+++ src/main/java/org/day11/files/WriteContentIntoaFileExample.java
@@ -1,15 +1,25 @@
 package org.day11.files;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WriteContentIntoaFileExample {
     public static void main(String[] args) {
+        FileWriter fileWriter;
         try {
-            FileWriter fileWriter = new FileWriter("C:\\Selenium_Training_PPT\\evening.txt");
-            fileWriter.write("This class is dedicated to Prhilistine Martyr");
-            fileWriter.write("Second line");
-            fileWriter.close();
+           File file=new File("C:\\Selenium_Training_PPT\\evening.txt");
+           if(file.exists()){
+               System.out.println("File already exist");
+
+           }
+            else {
+               fileWriter = new FileWriter(file);
+               fileWriter.write("This class is dedicated to Prhilistine Martyr");
+               fileWriter.write("Second line");
+               fileWriter.close();
+           }
+
         }catch (IOException ioException){
             ioException.printStackTrace();
         }
