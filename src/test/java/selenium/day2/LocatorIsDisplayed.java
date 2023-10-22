2cb8aae2fd06de600b2227fa376ac76af6fd234d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-22 10:25:11 +0530

    weekly batch commit - 21 Oct 2023 For each

---
+++ src/test/java/selenium/day2/LocatorIsDisplayed.java
@@ -0,0 +1,31 @@
+package selenium.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatorIsDisplayed {
+    public static void main(String[] args) throws Throwable{
+
+
+    WebDriverManager.chromedriver().setup();//
+    WebDriver driver = new ChromeDriver();
+    // Navigation method
+        driver.get("file:///C:/Users/shahn/OneDrive/Desktop/index.html");
+        driver.manage().window().maximize();
+    // Element by id
+    WebElement link = driver.findElement(By.partialLinkText("Official Page"));
+        Thread.sleep(2000);
+    String text = link.getText();
+        System.out.println(text);
+    // Get tag name
+        System.out.println("Tag name \t" + link.getTagName());
+        Thread.sleep(2000);
+        System.out.println("Link enable\t"+link.isEnabled());
+        System.out.println("Link displayed \t"+link.isDisplayed());
+        //link.click();
+        Thread.sleep(5000);
+        driver.quit();
+}}
