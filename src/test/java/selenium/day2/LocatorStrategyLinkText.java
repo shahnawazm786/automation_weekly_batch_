f335761e04f66a66432bec34eb012a47098b05f4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-29 10:33:36 +0530

    weekly batch commit - 28 Oct 2023 For each

--- src/test/java/selenium/day2/LocatorStrategyLinkText.java
+++ src/test/java/selenium/day2/LocatorStrategyLinkText.java
@@ -11,7 +11,7 @@ public class LocatorStrategyLinkText {
         WebDriverManager.chromedriver().setup();//
         WebDriver driver = new ChromeDriver();
         // Navigation method
-        driver.get("file:///C:/Users/shahn/OneDrive/Desktop/index.html");
+        driver.get("file:///C:/Users/shahn/OneDrive/Desktop/default.html");
         driver.manage().window().maximize();
         // Element by id
         WebElement link = driver.findElement(By.linkText("Selenium Official Page"));
