4c0cb30fd7e7ffa2f79dea65dd7d9226d8c724f3
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-29 13:45:58 +0530

    weekly batch commit - 28 Oct 2023 For each

--- src/main/java/practice/todolist/TodoListExample.java
+++ src/main/java/practice/todolist/TodoListExample.java
@@ -1,8 +1,21 @@
 package practice.todolist;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class TodoListExample {
     //https://react-cool-todo-app.netlify.app/
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
 
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://react-cool-todo-app.netlify.app/");
+
+        driver.quit();
     }
 }
