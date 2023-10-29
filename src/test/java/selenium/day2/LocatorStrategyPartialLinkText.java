9d580e2c042da15efb7051d34dc667638b3c09c4
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-10-29 10:33:47 +0530

    weekly batch commit - 28 Oct 2023 For each

--- src/test/java/selenium/day2/LocatorStrategyPartialLinkText.java
+++ src/test/java/selenium/day2/LocatorStrategyPartialLinkText.java
@@ -10,7 +10,7 @@ public class LocatorStrategyPartialLinkText {
         WebDriverManager.chromedriver().setup();//
         WebDriver driver = new ChromeDriver();
         // Navigation method
-        driver.get("file:///C:/Users/shahn/OneDrive/Desktop/index.html");
+        driver.get("file:///C:/Users/shahn/OneDrive/Desktop/default.html");
         driver.manage().window().maximize();
         // Element by id
         WebElement link = driver.findElement(By.partialLinkText("Official Page"));
