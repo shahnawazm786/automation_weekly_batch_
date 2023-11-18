fd846d6eb45d2cc51d3821e6577fb64c5115e1e6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-11-18 20:12:40 +0530

    weekly batch commit - 18 Nov 2023 For each

--- src/test/java/selenium/day6/FeedFormExample.java
+++ src/test/java/selenium/day6/FeedFormExample.java
@@ -20,34 +20,39 @@ public class FeedFormExample {
         driver.findElement(By.id("firstName")).sendKeys("Mohammad");
         driver.findElement(By.id("lastName")).sendKeys("Shahnawaz");
         driver.findElement(By.id("userEmail")).sendKeys("kazonline@gmail.com");
-        //driver.findElement(By.cssSelector("input[id=gender-radio-1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("label[for=gender-radio-1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("label[for=gender-radio-2]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("label[for=gender-radio-3]")).click();
         //userNumber
-        driver.findElement(By.id("userNumber")).sendKeys("6789012345");
-        //dateOfBirthInput
-        driver.findElement(By.id("dateOfBirthInput")).sendKeys(Keys.ENTER);
-        //div[text()='19']
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//div[text()='25']")).click();
-        //subjectsInput
-        driver.findElement(By.id("subjectsInput")).sendKeys("Math");
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//*[text()='Math']")).click();
-        //hobbies-checkbox-1
-        Thread.sleep(1000);
-        driver.findElement(By.id("hobbies-checkbox-1")).click();
-        //uploadPicture
-        Thread.sleep(1000);
-        driver.findElement(By.id("uploadPicture")).sendKeys("\\IntellijScalaWorkSpace\\WeeklyAutomationBatch\\image\\screenshot.png");
-        //currentAddress
-        Thread.sleep(1000);
-        driver.findElement(By.id("currentAddress")).sendKeys("South Delhi \n New Delhi \n 110017");
-        //react-select-3-input
-        driver.findElement(By.id("react-select-3-input")).click();
-        ////div[text()='Uttar Pradesh' and@id='react-select-3-option-1']
-        driver.findElement(By.xpath("//div[text()='Uttar Pradesh' and@id='react-select-3-option-1']")).click();
-        //submit
-        driver.findElement(By.id("submit")).click();
-        Thread.sleep(5000);
-        driver.quit();
+//        driver.findElement(By.id("userNumber")).sendKeys("6789012345");
+//        //dateOfBirthInput
+//        driver.findElement(By.id("dateOfBirthInput")).sendKeys(Keys.ENTER);
+//        //div[text()='19']
+//        Thread.sleep(1000);
+//        driver.findElement(By.xpath("//div[text()='25']")).click();
+//        //subjectsInput
+//        driver.findElement(By.id("subjectsInput")).sendKeys("Math");
+//        Thread.sleep(1000);
+//        driver.findElement(By.xpath("//*[text()='Math']")).click();
+//        //hobbies-checkbox-1
+//        Thread.sleep(1000);
+//        driver.findElement(By.id("hobbies-checkbox-1")).click();
+//        //uploadPicture
+//        Thread.sleep(1000);
+//        driver.findElement(By.id("uploadPicture")).sendKeys("\\IntellijScalaWorkSpace\\WeeklyAutomationBatch\\image\\screenshot.png");
+//        //currentAddress
+//        Thread.sleep(1000);
+//        driver.findElement(By.id("currentAddress")).sendKeys("South Delhi \n New Delhi \n 110017");
+//        //react-select-3-input
+//        driver.findElement(By.id("react-select-3-input")).click();
+//        ////div[text()='Uttar Pradesh' and@id='react-select-3-option-1']
+//        driver.findElement(By.xpath("//div[text()='Uttar Pradesh' and@id='react-select-3-option-1']")).click();
+//        //submit
+//        driver.findElement(By.id("submit")).click();
+//        Thread.sleep(5000);
+        //driver.quit();
     }
 }
