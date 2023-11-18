9b3ee30251915b91f4d8932996d55a30eb972171
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-11-18 21:16:13 +0530

    weekly batch commit - 18 Nov 2023 For each

--- src/test/java/selenium/day6/FeedFormExample.java
+++ src/test/java/selenium/day6/FeedFormExample.java
@@ -44,7 +44,8 @@ public class FeedFormExample {
         driver.findElement(By.cssSelector("label[for=hobbies-checkbox-2]")).click();
         Thread.sleep(1000);
         driver.findElement(By.cssSelector("label[for=hobbies-checkbox-3]")).click();
-
+        Thread.sleep(1000);
+        driver.findElement(By.id("uploadPicture")).sendKeys("");
 
         Thread.sleep(10000);
         driver.quit();
