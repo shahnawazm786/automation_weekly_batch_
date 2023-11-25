054952b675a426a2d8367e43e26b9bc22b2e62a7
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-11-25 11:33:08 +0530

    weekly batch commit - 25 Nov 2023 For each

---
+++ src/test/java/TestNG/day01/TestNGExample.java
@@ -0,0 +1,23 @@
+package TestNG.day01;
+
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertFalse;
+
+public class TestNGExample {
+    @Test
+    public void test001(){
+        System.out.println("Helloo!!! Test NG Method 1");
+    }
+    @Test
+    public void test002(){
+        System.out.println("Helloo!!! Test NG Method 2");
+        assertFalse(true);
+        System.out.println("Hello assertion failed");
+    }
+    @Test
+    public void test003(){
+        System.out.println("Helloo!!! Test NG Method 3");
+    }
+}
