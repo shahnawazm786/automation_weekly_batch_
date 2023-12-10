4ef3e8b239912110f9bacf5239140fed87fffff1
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-10 10:40:34 +0530

    weekly batch commit - 10 Dec 2023 For each

--- src/test/java/steps/MyStepdefs1.java
+++ src/test/java/steps/MyStepdefs1.java
@@ -1,7 +1,12 @@
 package steps;
 
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.List;
 
 public class MyStepdefs1 {
     @Then("i direct to the user dashboard and found {int} profile")
@@ -22,5 +27,39 @@ public class MyStepdefs1 {
     @Given("dircted to approval dashboard")
     public void dirctedToApprovalDashboard() {
         System.out.println("Approval");
+    }
+
+    @When("I am verifying the user {string}")
+    public void iAmVerifyingTheUser(String arg0) {
+        System.out.println("user name"+ arg0);
+    }
+
+    @Given("I am going to verify password {string}")
+    public void iAmGoingToVerifyPassword(String arg0) {
+        System.out.println("Password \t"+arg0);
+    }
+
+    @Given("I have the following books in the store")
+    public void iHaveTheFollowingBooksInTheStore(DataTable books) {
+        System.out.println(books);
+        List<List<String>>  rows=books.asLists(String.class);
+        for(List<String> row : rows){
+            System.out.println(row.get(1));
+        }
+    }
+
+    @When("I search for books by author Erik Larson")
+    public void iSearchForBooksByAuthorErikLarson() {
+        System.out.println("Frankling book name");
+    }
+
+    @Then("I find {int} books")
+    public void iFindBooks(int arg0) {
+        System.out.println("No of books \t"+arg0);
+    }
+
+    @And("The price is {double}")
+    public void thePriceIs(double price) {
+        System.out.println("Price is "+price);
     }
 }
