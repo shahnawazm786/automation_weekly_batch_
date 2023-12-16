b5a59e104143cb6bb094b42b2e56912d7fc5bafa
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-16 19:20:33 +0530

    weekly batch commit - 16 Dec 2023 For each

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,17 +1,23 @@
 package steps;
 
+import Utility.ReadJsonFile;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebDriver;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Hooks {
     protected WebDriver driver;
     @Before
-    public void setup(){
+    public void setup() throws IOException, ParseException {
         System.out.println(System.getenv("url"));
         System.out.println(System.getenv("browserName"));
         System.out.println(System.getenv("environment"));
         System.out.println("Setup file");
+        System.out.println(ReadJsonFile.JsonData("url"));
     }
     @After
     public void tearDown(){
