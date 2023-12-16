e32d4bf955736c00a466e2307eacf5154b80f157
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-16 14:12:29 +0530

    weekly batch commit - 16 Dec 2023 For each

---
+++ src/main/java/Utility/SeleniumFunctions.java
@@ -0,0 +1,33 @@
+package Utility;
+
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+
+public class SeleniumFunctions {
+    /**
+     * Mohammad Shahnawaz
+     * @param driver
+     */
+    public static void maximizeWindow(WebDriver driver){
+        driver.manage().window().maximize();
+        }
+
+    /**
+     * Mohammad Shahnawaz
+     * @param driver
+     * @param timeout in seconds
+     */
+    public static  void ImplicitWait(WebDriver driver,long timeout){
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout));
+        }
+
+    /**
+     * Mohammad Shahnawaz
+     * @param driver
+     * @param url
+     */
+    public static void webURL(WebDriver driver,String url){
+
+        }
+}
