71db898a48407fe072f16f192de566a9264a0519
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-16 14:12:36 +0530

    weekly batch commit - 16 Dec 2023 For each

---
+++ src/test/java/steps/SetupDriver.java
@@ -0,0 +1,24 @@
+package steps;
+
+import Utility.EBrowser;
+import Utility.SeleniumFunctions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SetupDriver {
+    protected WebDriver driver;
+    public WebDriver DriverManager(String browserName){
+        if(browserName.equalsIgnoreCase(EBrowser.CHROME.toString())){
+            System.out.println("Chrome Browser");
+            ChromeOptions options=new ChromeOptions();
+            options.addArguments("--maximized--");
+            driver=new ChromeDriver(options);
+            SeleniumFunctions.maximizeWindow(driver);
+            SeleniumFunctions.ImplicitWait(driver,10);
+
+
+        }
+        return driver;
+    }
+}
