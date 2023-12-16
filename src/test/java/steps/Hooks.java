9bd7f968bdb3f9c3fc632eaf22b6f979af4cca5f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-16 20:26:15 +0530

    weekly batch commit - 16 Dec 2023 For each

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -4,10 +4,12 @@ import Utility.ReadJsonFile;
 import Utility.ReadingPropertiesFile;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import org.json.simple.parser.ParseException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Hooks {
@@ -21,9 +23,21 @@ public class Hooks {
         System.out.println(ReadJsonFile.JsonData("url"));
         System.out.println(ReadingPropertiesFile.getPropertyValue("username"));
         System.out.println(ReadingPropertiesFile.getPropertyValue("password"));
+        driver=SetupDriver.DriverManager(System.getenv("browserName"));
     }
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
         System.out.println("Tear down");
+        if(scenario.isFailed()){
+            TakesScreenshot screenshot=(TakesScreenshot) driver;
+            byte[] src=screenshot.getScreenshotAs(OutputType.BYTES);
+            scenario.attach(src,"img/png","error_file");
+        }
+        if(driver!=null){
+            driver.manage().deleteAllCookies();
+            driver.quit();
+        }
+
     }
+
 }
