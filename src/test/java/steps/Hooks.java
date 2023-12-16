619b1beac63838acdad96958dae1a44dff66e08a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-16 22:49:38 +0530

    weekly batch commit - 16 Dec 2023 For each

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,43 +1,41 @@
 package steps;
 
-import Utility.ReadJsonFile;
-import Utility.ReadingPropertiesFile;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import org.json.simple.parser.ParseException;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 public class Hooks {
-    protected WebDriver driver;
+    public static WebDriver driver;
     @Before
-    public void setup() throws IOException, ParseException {
-        System.out.println(System.getenv("url"));
-        System.out.println(System.getenv("browserName"));
-        System.out.println(System.getenv("environment"));
-        System.out.println("Setup file");
-        System.out.println(ReadJsonFile.JsonData("url"));
-        System.out.println(ReadingPropertiesFile.getPropertyValue("username"));
-        System.out.println(ReadingPropertiesFile.getPropertyValue("password"));
-        driver=SetupDriver.DriverManager(System.getenv("browserName"));
+    public void setUpDriver(){
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
     }
     @After
-    public void tearDown(Scenario scenario){
-        System.out.println("Tear down");
-        if(scenario.isFailed()){
-            TakesScreenshot screenshot=(TakesScreenshot) driver;
-            byte[] src=screenshot.getScreenshotAs(OutputType.BYTES);
-            scenario.attach(src,"img/png","error_file");
-        }
-        if(driver!=null){
-            driver.manage().deleteAllCookies();
-            driver.quit();
-        }
+    public void tearDownDriver(Scenario scenario) throws IOException {
+        if(scenario.isFailed())
+        {
 
+            final byte[] screenshot=((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png", "Errors");
+
+            //File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            //byte[] fileContent = FileUtils.readFileToByteArray(screenshot);
+            //scenario.attach(fileContent, "image/png", "screenshot");
+
+        }
+        driver.manage().deleteAllCookies();
+        driver.quit();
     }
-
 }
