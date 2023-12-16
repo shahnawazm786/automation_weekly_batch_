888a884bff2cb07a8b84ba0c9a978052cd2ae126
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-16 20:26:21 +0530

    weekly batch commit - 16 Dec 2023 For each

--- src/test/java/steps/SetupDriver.java
+++ src/test/java/steps/SetupDriver.java
@@ -1,14 +1,19 @@
 package steps;
 
 import Utility.EBrowser;
+import Utility.ReadJsonFile;
 import Utility.SeleniumFunctions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.IOException;
+
 public class SetupDriver {
-    protected WebDriver driver;
-    public WebDriver DriverManager(String browserName){
+    static protected WebDriver driver;
+    public static WebDriver DriverManager(String browserName) throws IOException, ParseException {
         if(browserName.equalsIgnoreCase(EBrowser.CHROME.toString())){
             System.out.println("Chrome Browser");
             ChromeOptions options=new ChromeOptions();
@@ -17,8 +22,7 @@ public class SetupDriver {
             //SeleniumFunctions.maximizeWindow(driver);
             SeleniumFunctions.ImplicitWait(driver,10);
             String url=System.getProperty("url");
-            SeleniumFunctions.webURL(driver,"");
-
+            SeleniumFunctions.webURL(driver, ReadJsonFile.JsonData("url"));
         }
         return driver;
     }
