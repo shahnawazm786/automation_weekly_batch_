630ea839ff7d1b87811b969d3bbb0dcf6ebc1650
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-16 10:27:59 +0530

    weekly batch commit - 10 Dec 2023 For each

---
+++ src/test/java/StepDefs/OrangeHRMSLoginSteps.java
@@ -0,0 +1,29 @@
+package StepDefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class OrangeHRMSLoginSteps {
+    @Given("Open the login page")
+    public void openTheLoginPage() {
+
+    }
+
+    @When("Enter user name in user text box")
+    public void enterUserNameInUserTextBox() {
+    }
+
+    @And("Enter password in password text box")
+    public void enterPasswordInPasswordTextBox() {
+    }
+
+    @And("I clcik on submit button")
+    public void iClcikOnSubmitButton() {
+    }
+
+    @Then("I verify the user dashboard")
+    public void iVerifyTheUserDashboard() {
+    }
+}
