f19a9c2a1096fb3b42b3d0618dfd4f3de4102448
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-16 10:27:39 +0530

    weekly batch commit - 10 Dec 2023 For each

---
+++ src/test/java/Acceptance/acceptance.java
@@ -0,0 +1,14 @@
+package Acceptance;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/OrangeFeatures",
+        glue = "StepDefs",
+        tags = "@OrangeHRMSLogin"
+)
+public class acceptance {
+}
