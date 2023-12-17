bbbbbfb3797e5d380f8cf516c0628ecef1369bf7
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-17 10:30:06 +0530

    weekly batch commit - 16 Dec 2023 For each

--- src/test/java/Acceptance/acceptance.java
+++ src/test/java/Acceptance/acceptance.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/OrangeFeatures",
         glue = "StepDefs",
-        tags = "@OrangeHRMSLogin"
+        tags = "@OrangeHRMSLogin",
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class acceptance {}
