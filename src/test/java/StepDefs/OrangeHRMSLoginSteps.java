fde3ea89669bb59e30267730628f6f42c484d6ee
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-12-17 10:31:29 +0530

    weekly batch commit - 16 Dec 2023 For each

--- src/test/java/StepDefs/OrangeHRMSLoginSteps.java
+++ src/test/java/StepDefs/OrangeHRMSLoginSteps.java
@@ -1,41 +1,57 @@
-package steps;
+package StepDefs;
+
 
 import Pages.OrnageHRMSLoginPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import steps.Hooks;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
 
 public class OrangeHRMSLoginSteps {
 
-WebDriver driver= Hooks.driver;
+//WebDriver driver=new Apps_Hooks().driver; object
+    WebDriver driver=Apps_Hooks.driver;
     OrnageHRMSLoginPage loginPage=new OrnageHRMSLoginPage(driver);
     @Given("Open the login page")
     public void openTheLoginPage() {
-        System.out.println("Login page");
-
     }
 
     @When("Enter user name in user text box")
-    public void enterUserNameInUserTextBox() {
+    public void enterUserNameInUserTextBox() throws IOException {
         loginPage.enterUserName();
+
 
     }
 
     @And("Enter password in password text box")
-    public void enterPasswordInPasswordTextBox() {
+    public void enterPasswordInPasswordTextBox() throws IOException {
+
         loginPage.enterPassword();
+
     }
 
     @And("I click on submit button")
     public void iClcikOnSubmitButton() {
+
         loginPage.clickOnSubmit();
+
     }
 
     @Then("I verify the user dashboard")
-    public void iVerifyTheUserDashboard() {
+    public void iVerifyTheUserDashboard() throws IOException {
+    //String dashboardText=driver.findElement(By.cssSelector("h6.oxd-topbar-header-breadcrumb-module"))
+     //       .getText();
+
+      //  assertEquals("Dashboard",dashboardText);
         loginPage.verifyDashboardLabel();
+
+
     }
 }
